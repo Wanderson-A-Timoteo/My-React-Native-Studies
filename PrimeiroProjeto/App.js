691615ae44b21a0ec20face3580b7c83d9fc5927
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2022-01-10 17:30:14 -0400

    Concluído primeiro app em React Native, Olá Mundo!

---
+++ PrimeiroProjeto/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App: () => Node = () => {
+  return (
+    <SafeAreaView>
+      <Text>Olá, Mundo!</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
